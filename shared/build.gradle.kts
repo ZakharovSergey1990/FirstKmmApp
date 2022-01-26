8429297aa2cf6548708fd582589917122b00e16c
user209353 <info@salvadorvdali.ru>
2022-01-26 16:11:43 +0100

    update ktor dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,16 +1,23 @@
 plugins {
+    id("com.squareup.sqldelight")
+    kotlin("plugin.serialization") version "1.6.10"
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+   // kotlin("jvm") version "1.6.0"
+   // id ("org.jetbrains.kotlin.jvm") version "1.6.0"
 }
 
 version = "1.0"
+
+
+
 
 kotlin {
     android()
     iosX64()
     iosArm64()
-    //iosSimulatorArm64() sure all ios dependencies support this target
+    iosSimulatorArm64()
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -23,37 +30,91 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+
+        val ktorVersion = "1.6.7"
+        val napierVersion = "2.3.0"
+        val commonMain by getting {
+            dependencies {
+
+
+              //  //ktor /
+               implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-logging:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+
+
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.6.10")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+
+                //implementation ("io.ktor:ktor-client-json:1.0.0")
+             //   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+
+
+                implementation("com.squareup.sqldelight:runtime:1.5.3")
+                implementation("com.squareup.sqldelight:coroutines-extensions:1.5.3")
+
+                //di
+
+                implementation("org.kodein.di:kodein-di:7.10.0")
+
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+
+                implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                //  implementation ("io.ktor:ktor-client-json-jvm:1.0.0")
+                //   implementation ("io.ktor:ktor-client-android:1.0.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+                implementation("com.squareup.sqldelight:android-driver:1.5.3")
+            }
+        }
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
                 implementation("junit:junit:4.13.2")
             }
         }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        //val iosSimulatorArm64Main by getting
+        val iosX64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+            }
+        }
+        val iosArm64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+            }
+        }
+        val iosSimulatorArm64Main by getting {
+            dependencies {
+               implementation("io.ktor:ktor-client-ios:$ktorVersion")
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+            }
+        }
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            //iosSimulatorArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-        //val iosSimulatorArm64Test by getting
+        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            //iosSimulatorArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
