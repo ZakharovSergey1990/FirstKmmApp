21a4c19d28c0e8f2700ad7ddfd473e679445f13a
s.zakharov <s.zakharov@baibol.ru>
2022-01-27 06:24:51 +0300

    Change ios target

--- androidApp/src/main/java/com/example/firstkmmapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/firstkmmapp/android/MainActivity.kt
@@ -4,20 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firstkmmapp.Greeting
 import android.widget.TextView
-import com.example.firstkmmapp.data.DriverFactory
+import com.example.firstkmmapp.data.*
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-val driver = DriverFactory.createDriver(application)
+val driver = DriverFactory(application)
         val userDataSource = UserDataSourceImpl(driver)
 
         val tv: TextView = findViewById(R.id.text_view)
         tv.text = greet()
+
+        MainScope().launch {
+            userDataSource.insertUser(user = User(1, "sdf", "adsfg", "sdgf",
+                Address("sdf", "asdf", "sd", "sdf", Geo("sdf", "sdf")),
+                "123213", "dsf@", Company("sdef", "sdf", "sdfg")
+            ))
+           tv.text = userDataSource.getAllUsers().toString()
+        }
     }
 }
--- shared/src/iosMain/kotlin/com/example/firstkmmapp/data/DriverFactory.kt
+++ shared/src/iosMain/kotlin/com/example/firstkmmapp/data/DriverFactory.kt
@@ -1,9 +1,11 @@
 package com.example.firstkmmapp.data
 
+import com.example.db.TestDatabase
+import com.example.firstkmmapp.KMMContext
 import com.squareup.sqldelight.db.SqlDriver
-import testdatabase.TestDatabase
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
-actual class DriverFactory {
+actual class DriverFactory actual constructor(context: KMMContext) {
     actual fun createDriver(): SqlDriver {
         return NativeSqliteDriver(TestDatabase.Schema, "TestDatabase.db")
     }
