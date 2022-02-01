a714a756328d4b229c5d0f1a3d1a35294d7b6713
user209353 <info@salvadorvdali.ru>
2022-02-01 08:16:41 +0100

    provide flow in ios

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -8,11 +8,27 @@ struct ContentView: View {
 
     func load(){
 
-               let repo = UserRepositoryImpl.init(userDataSource: UserDataSourceImpl.init(driver: DriverFactory.init(context: NSObject.init())), testHttpApi: TestHttpApiImpl.init())
+        let userDataSource = UserDataSourceImpl(driver: DriverFactory.init(context: NSObject.init()))
+        
+               let repo = UserRepositoryImpl.init(userDataSource: userDataSource, testHttpApi: TestHttpApiImpl.init())
+    
+        
+        
+        userDataSource.getAllUsersAsFlow().collect(collector: Observer(callback: {value in
+           
+            let result = value as? [User]
+        
+            print(result)
+        
+            xxx = "size = \(result[])"
+        }), completionHandler: {data, error in
+            print(data)
+        })
+        
        xxx = "start get Users"
         repo.getAllUsers(completionHandler: {result, error in
             if let result = result{
-                self.xxx = "\(result.count)"
+                self.xxx = "kfejgblakejfgb"
             } else if let error = error{
                 self.xxx = "Error"
             }
@@ -29,4 +45,20 @@ struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
 	}
+}
+
+typealias Collector = Kotlinx_coroutines_coreFlowCollector
+
+class Observer: Collector{
+    
+    let callback:(Any?) -> Void
+    
+    init(callback: @escaping (Any?) -> Void){
+        self.callback = callback
+    }
+
+    func emit(value: Any?, completionHandler: @escaping (KotlinUnit?, Error?) -> Void) {
+        callback(value)
+        completionHandler(KotlinUnit(), nil)
+    }
 }
---
+++ iosApp/iosApp/UsersViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  UsersViewModel.swift
+//  iosApp
+//
+//  Created by user209353 on 1/31/22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+class UserViewModel : ObservableObject {
+    
+
+    
+}
